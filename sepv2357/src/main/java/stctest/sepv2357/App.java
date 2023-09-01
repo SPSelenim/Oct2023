979419c06f1a2ebd4a3d4309547e1d976e73f571
spselenim <pawal.satish@gmail.com>
2023-09-01 17:07:07 +0530

    first commit

---
+++ sepv2357/src/main/java/stctest/sepv2357/App.java
@@ -0,0 +1,13 @@
+package stctest.sepv2357;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sepv2357/src/test/java/stctest/sepv2357/NewTest2.java
@@ -0,0 +1,25 @@
+package stctest.sepv2357;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+  System.out.println("S T A R T E D ******************");
+  WebDriverManager.firefoxdriver().setup();
+  WebDriver driver=new FirefoxDriver();
+  driver.get("D:\\Satish\\05DemoApps\\demo.html");
+  driver.manage().window().maximize();
+  String a=driver.findElement(By.xpath("//a[@name='Mylink']")).getAttribute("href");
+  System.out.println("Get Attribute for href : " + a);
+  String myLink=driver.findElement(By.xpath("//b[contains(text(),'Hello')]")).getText();
+ System.out.println("******My Link ********"+myLink);
+  driver.quit();
+  }
+}
