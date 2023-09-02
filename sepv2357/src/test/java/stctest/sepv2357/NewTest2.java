4a5bdeba7dcca8d21291d0c6561a54594a721416
spselenim <pawal.satish@gmail.com>
2023-09-02 14:46:30 +0530

    14

--- sepv2357/src/test/java/stctest/sepv2357/NewTest2.java
+++ sepv2357/src/test/java/stctest/sepv2357/NewTest2.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 //New Test 2 Parth
 //New SP
+//14
 public class NewTest2 extends MyLib{
   @Test
   public void f() throws InterruptedException, IOException {
